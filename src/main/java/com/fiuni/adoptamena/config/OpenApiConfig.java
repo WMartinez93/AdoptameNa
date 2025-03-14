172812b558a94d1c549abd2565b6c40b5afece15
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2025-03-14 01:04:00 -0300

    fix: agregar configuración de servidor para la API en OpenApiConfig

--- src/main/java/com/fiuni/adoptamena/config/OpenApiConfig.java
+++ src/main/java/com/fiuni/adoptamena/config/OpenApiConfig.java
@@ -4,23 +4,29 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("AdoptameNa API").version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .name("bearerAuth")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info().title("AdoptameNa API").version("1.0"))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new io.swagger.v3.oas.models.Components()
+                                                .addSecuritySchemes("bearerAuth",
+                                                                new SecurityScheme()
+                                                                                .name("bearerAuth")
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")))
+                                .servers(List.of(
+                                                new Server().url("https://adoptamena-api.rodrigomaidana.com")
+                                                                .description("Producción")));
+        }
 }
